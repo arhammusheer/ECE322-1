6a7ed99eb06fe8aafa69e5587b9021c1f1921a93
Ben Short <ben.is.not.short@gmail.com>
2019-09-25 13:30:44 -0400

    Initial Upload

---
+++ deck.c
@@ -0,0 +1,56 @@
+#include "deck.h"
+
+
+int shuffle(){
+	for(int suit = 0; suit < 4; suit++){
+		for(
+	}
+	return 0;
+}
+
+
+int deal_player_cards(struct player* target){
+	
+	return 0;
+}
+
+
+struct card* next_card(){
+
+	
+}
+
+
+size_t deck_size(){
+	
+	
+}
+
+char suit(int s){
+	switch(s){
+	case 0: return 'C';
+	case 1: return 'D';
+	case 2: return 'H';
+	case 3: return 'S';
+	default: return '\0';
+	}
+}
+
+char* rank(int r){
+	
+	switch(r){
+	case 1:  
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+	case 13:
+	}	
+}

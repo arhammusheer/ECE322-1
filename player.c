06d4b418a1b1c98b2e8871dcb9aecae962e6354d
Bradley Zylstra <bbzylstra@randolphcollege.edu>
2019-09-25 13:49:48 -0400

    added interface code to player.c

---
+++ player.c
@@ -0,0 +1,37 @@
+#include "player.h"
+
+int add_card(struct player* target, struct card* new_card){
+	return 0;
+}
+
+int remove_card(struct player* target, struct card* old_card){
+	return 0;
+}
+
+char check_add_book(struct player* target){
+	return 0;
+}
+
+int search(struct player* target, char rank){
+	return 0;
+}
+
+int transfer_cards(struct player* src, struct player* dest, char rank){
+	return 0;
+}
+
+int game_over(struct player* target){
+	return 0;
+}
+
+int reset_player(struct player* target){
+	return 0;
+}
+
+char computer_play(struct player* target){
+	return 0;
+}
+
+char user_play(struct player* target){
+	return 0;
+}

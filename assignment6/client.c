cfe45995123df4ab83b09ce5bb4c4fc8bc3b9377
Brad <bbzylstra@randolphcollege.edu>
2019-12-08 18:01:20 -0500

    Added clientc changes

--- assignment6/client.c
+++ assignment6/client.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "csapp.h"
+#include <regex.h>
+
 
 int main(int argc, char **argv) 
 {
@@ -22,8 +24,53 @@ int main(int argc, char **argv)
     host = "localhost";
     port = "8080";
 
-    clientfd = Open_clientfd(host, port);
+    char * buffer1 = malloc(100);
+    char * buffer2= malloc(100);
+    sprintf(buffer1, "<param><value><double>%s</double></value></param>\r\n", num1);
+    sprintf(buffer2, "<param><value><double>%s</double></value></param>\r\n", num2);
+    char * body= malloc(400);
+    strcat(body, "<?xml version=\"1.0\"?>\r\n");
+    strcat(body, "<methodCall>\r\n");
+    strcat(body, "<methodName>sample.addmultiply</methodName>\r\n");
+    strcat(body, "<params>\r\n");
+    strcat(body, buffer1);
+    strcat(body, buffer2);
+    strcat(body, "</params>\r\n");
+    strcat(body, "</methodCall>\r\n");
 
+    char * header=malloc(400);
+    strcat(header, "POST /RPC2 HTTP/1.1\r\n");
+    strcat(header, "Content-Type: text/xml\r\n");
+    strcat(header, "User-Agent: XML-RPC.NET\r\n");
+    char * buffer3 = malloc(100);
+    sprintf(buffer3, "Content-Length: %lu\r\n", strlen(body));
+    strcat(header, buffer3);
+    strcat(header, "Expect: 100-continue\r\n");
+    strcat(header, "Connection: Keep-Alive\r\n");
+    strcat(header, "Host: localhost:8080\r\n\r\n");
+
+    char * message=malloc(900);
+    strcat(message, header);
+    strcat(message, body);
+
+
+    char * serverreply[500];
+    clientfd = Open_clientfd(host, port);
+    send(clientfd, message, strlen(message), 0);
+    recv(clientfd, serverreply, 500, 0);
+    printf(serverreply);
+    strcat(serverreply, "\0");
+
+    regex_t regex;
+    int reti;
+    regmatch_t rm[2];
+    reti = regcomp(&regex, "<double>(.*)</double>",REG_EXTENDED);
+    reti = regexec(&regex, serverreply, 2, rm, 0);
+    printf("%i", reti);
+    printf("Text: %.*s", (int)(rm[1].rm_eo - rm[1].rm_so), serverreply + rm[1].rm_so);
+    //char msgbuf[100];
+    //regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+    //fprintf(stderr, "Regex match failed: %s\n", msgbuf);
     /* 
 	Write your code here.
 	Recommend to use the Robust I/O package.
--- assignment6/client.py
+++ assignment6/client.py
@@ -4,5 +4,6 @@ import sys
 
 proxy = xmlrpclib.ServerProxy("http://localhost:8080/RPC2")
 return_list = proxy.sample.addmultiply(float(sys.argv[1]),float(sys.argv[2]))
+print proxy
 print return_list[0], " ", return_list[1]
 
